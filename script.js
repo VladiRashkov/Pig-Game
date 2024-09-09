6acf31272cb2b292ec19e6b4b03184765652fc92
Vladi Rashkov <vladislav.rashkoff@gmail.com>
2024-09-09 13:23:29 +0300

    added logic

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
 'use strict';
+// selectin elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let current_score = 0;
+let activePlayer = 0;
+let playing = true;
+
+const swictPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    current_score = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
+
+btnRoll.addEventListener('click', function() {
+    if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    if (dice !== 1) {
+        current_score += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = current_score;
+
+
+    } else {
+        swictPlayer()
+    }
+    }
+});
+
+
+btnHold.addEventListener('click', function() {
+    if (playing) {
+    scores[activePlayer] += current_score;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+    if (scores[activePlayer]>=100){
+        playing = false;
+        diceEl.classList.add('hidden');
+        document.querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+
+
+    }else{
+        swictPlayer();
+    }
+}
+}
+);
